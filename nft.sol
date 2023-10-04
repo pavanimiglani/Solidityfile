f446dbd81a7b4d55014d18da9111f1b70f350e52
pavanimiglani <145712518+pavanimiglani@users.noreply.github.com>
2023-10-03 23:43:03 -0400

    Add files via upload

---
+++ nft.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint(address to, uint256 tokenId) external onlyOwner {
+        _mint(to, tokenId);
+    }
+} 
+
+
